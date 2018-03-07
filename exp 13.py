485c1f3bfddec6e4c5b9b6f9ed9ed94a428ae7f2
Mohanasundar.M <mmsmohanece@gmail.com>
2018-03-07 17:44:57 +0530

    Update exp 13.py

--- exp 13.py
+++ exp 13.py
@@ -1,10 +1,10 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter a no: "))
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
-           print("is not a prime number")
+           print("is not a prime")
            break
    else:
        print("is a prime number")
 else:
-   print("is not a prime number")
+   print("is not a prime")
