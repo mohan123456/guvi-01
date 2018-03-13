df4971501689ac57ebfccae5b5bde44713286580
Mohanasundar.M <mmsmohanece@gmail.com>
2018-03-13 17:17:02 +0530

    Create amstrong interval.py

---
+++ amstrong interval.py
@@ -0,0 +1,13 @@
+#amstrong interval
+a = int(input("Enter lower range : "))
+b = int(input("Enter upper range : "))
+for num in range(a, b + 1):
+  order = len(str(num))
+  sum = 0
+  temp = num
+  while temp > 0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+  if num == sum:
+       print(num)
