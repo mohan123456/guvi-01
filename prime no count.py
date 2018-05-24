837204a7fe9654f68cd5eb9358fac597a779b892
Mohanasundar.M <mmsmohanece@gmail.com>
2018-05-24 11:30:36 +0530

    Create prime no count.py

---
+++ prime no count.py
@@ -0,0 +1,10 @@
+b,r=map(int,input("Enter upper limit: ").split(" "))
+d=[]
+for a in range(b,r):
+    k=0
+    for i in range(2,a):
+        if(a%i==0):
+            k=k+1
+    if(k<=0):
+        d.append(a)
+print(len(d))       
